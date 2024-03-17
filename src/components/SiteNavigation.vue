86e35a9432a49121665f701388a0aa1a2a0c6396
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2024-03-17 22:50:16 +0100

    :tada: Lesson 4

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -4,21 +4,51 @@
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
       <RouterLink :to="{ name: 'home' }">
-          <div class="flex items-center gap-3 flex-1">
-            <i class="fa-solid fa-sun text-2xl"></i>
-            <p class="text-2xl">The local weather</p>
-          </div>
+        <div class="flex items-center gap-3 flex-1">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The local weather</p>
+        </div>
       </RouterLink>
       <div class="flex gap-3 flex-1 justify-end">
-        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+         @click="toggleModal"
+          ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
       </div>
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            This is the app about weather that you have more information about.
+          </p>
+          <h2 class="text-2xl">How it work:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar
+            </li>
+            <li>Click to the result of your search.</li>
+            <li>See the result.</li>
+          </ol>
+          <h3 class="text-2xl mb-1">Removing City:</h3>
+          <p class="mb-4">Just click to remove.</p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
-import { RouterLink } from "vue-router"
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
 
 <style lang="scss" scoped></style>
