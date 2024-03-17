782a65aaa729c9bbb5fbdbfa65b9a2177ebaeec4
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2024-03-17 21:18:14 +0100

    :tada: Lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <div class="flex flex-col min-h-screen font-Embed-code bg-weather-primary">
+    <SiteNavigation />
+    <routerView />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
 
 <style lang="scss" scoped></style>
